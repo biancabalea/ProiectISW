8efd6f67051b32f0c4712a189a01001be5e109ea
antoniadineti <126197493+antoniadineti@users.noreply.github.com>
2024-03-09 13:11:21 +0200

    v3

--- platformaHR/src/main/java/proiect/platformaHR/entity/Departament.java
+++ platformaHR/src/main/java/proiect/platformaHR/entity/Departament.java
@@ -2,6 +2,9 @@ package proiect.platformaHR.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="departament")
 @SequenceGenerator(name="dept_seq", initialValue = 4)
@@ -12,4 +15,33 @@ public class Departament {
     private Integer id;
     @Column(name="dept_name")
     private String name;
+    @OneToMany(mappedBy = "departament", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Post> postList = new ArrayList<>();
+
+    public Departament() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Post> getPostList() {
+        return postList;
+    }
+
+    public void setPostList(List<Post> postList) {
+        this.postList = postList;
+    }
 }
--- platformaHR/src/main/java/proiect/platformaHR/entity/Post.java
+++ platformaHR/src/main/java/proiect/platformaHR/entity/Post.java
@@ -12,4 +12,34 @@ public class Post {
     private Integer id;
     @Column(name="post_name")
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "post_dept_id", referencedColumnName = "dept_id")
+    private Departament departament;
+
+    public Post() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Departament getDepartament() {
+        return departament;
+    }
+
+    public void setDepartament(Departament departament) {
+        this.departament = departament;
+    }
 }
