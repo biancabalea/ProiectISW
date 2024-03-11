cf8e41a95c283e380871784fc63d86c5fffb17d6
antoniadineti <126197493+antoniadineti@users.noreply.github.com>
2024-03-11 12:02:59 +0200

    v10

--- platformaHR/src/main/java/proiect/platformaHR/config/SecurityConfiguration.java
+++ platformaHR/src/main/java/proiect/platformaHR/config/SecurityConfiguration.java
@@ -2,12 +2,15 @@ package proiect.platformaHR.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +23,14 @@ public class SecurityConfiguration {
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
     @Bean
      public BCryptPasswordEncoder encoder(){
