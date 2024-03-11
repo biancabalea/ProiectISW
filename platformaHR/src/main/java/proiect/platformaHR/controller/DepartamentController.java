c66ee47b1dc4b9ce99b3ea93c6471ef76e1be8b0
antoniadineti <126197493+antoniadineti@users.noreply.github.com>
2024-03-11 10:15:36 +0200

    v7

---
+++ platformaHR/src/main/java/proiect/platformaHR/controller/DepartamentController.java
@@ -0,0 +1,37 @@
+package proiect.platformaHR.controller;
+
+import org.springframework.web.bind.annotation.*;
+import proiect.platformaHR.entity.Departament;
+import proiect.platformaHR.repository.DepartamentRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/departaments")
+public class DepartamentController {
+    private DepartamentRepository departamentRepository;
+
+    public DepartamentController(DepartamentRepository departamentRepository) {
+        this.departamentRepository = departamentRepository;
+    }
+
+    @GetMapping
+    public List<Departament> getAllDepartement() {
+        return departamentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Departament getDepartament (@PathVariable("id") Integer id) {
+        return departamentRepository.findById(id).get();
+    }
+
+    @PostMapping
+    public Departament createDepartament(@RequestBody Departament departament){
+        return departamentRepository.save(departament);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDepartament(@PathVariable("id") Integer id) {
+        departamentRepository.deleteById(id);
+    }
+}
