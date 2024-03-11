4c2a35eef58a47e6dfdaf8a444d29a68ba78e299
antoniadineti <126197493+antoniadineti@users.noreply.github.com>
2024-03-11 09:58:29 +0200

    v5

---
+++ platformaHR/src/main/java/proiect/platformaHR/controller/PostController.java
@@ -0,0 +1,24 @@
+package proiect.platformaHR.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import proiect.platformaHR.entity.Post;
+import proiect.platformaHR.repository.PostRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    private final PostRepository postRepository;
+
+    public PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+}
