61e21452e60d09860682c55fd28e57f0964b743e
antoniadineti <126197493+antoniadineti@users.noreply.github.com>
2024-03-12 11:43:01 +0200

    v15

--- platformaHR/src/main/java/proiect/platformaHR/config/SecurityConfiguration.java
+++ platformaHR/src/main/java/proiect/platformaHR/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package proiect.platformaHR.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -10,21 +11,41 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity (prePostEnabled = true)
 public class SecurityConfiguration {
     @Bean
-    public UserDetailsService userDetailsService(BCryptPasswordEncoder encoder){
-        UserDetails user = User.builder()
-                .username("user")
-                .password(encoder.encode("userPass"))
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager userDetailManager = new JdbcUserDetailsManager(dataSource);
+        /*userDetailManager.createUser(createUser());
+        userDetailManager.createUser(createAdmin());*/
+        return userDetailManager;
     }
 
+    private UserDetails createUser() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(encoder().encode("userPass"))
+                .roles("USER")
+                .build();
+        return user;
+    }
+
+    private UserDetails createAdmin(){
+        UserDetails user = User.builder()
+                .username("admin")
+                .password(encoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return user;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
