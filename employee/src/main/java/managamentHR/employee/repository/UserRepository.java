6b186486ed1be063e72b3678e6ffb1090f099e7c
antoniadineti <126197493+antoniadineti@users.noreply.github.com>
2024-03-31 19:40:38 +0300

    varianta finala pentru backend
    consider ca am indeplinit cerintele prin acest cod si aceasta este varianta finala

--- employee/src/main/java/managamentHR/employee/repository/UserRepository.java
+++ employee/src/main/java/managamentHR/employee/repository/UserRepository.java
@@ -3,5 +3,8 @@ package managamentHR.employee.repository;
 import managamentHR.employee.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+
 }
