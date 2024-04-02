76dc40fc413c288562ccd4e57b321377abd8c77d
biancabalea <126229336+biancabalea@users.noreply.github.com>
2024-04-02 09:55:28 +0300

    Delete platformaHR directory

--- platformaHR/src/main/java/proiect/platformaHR/repository/DepartamentRepository.java
+++
@@ -1,8 +0,0 @@
-package proiect.platformaHR.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import proiect.platformaHR.entity.Departament;
-@Repository
-public interface DepartamentRepository extends JpaRepository<Departament, Integer> {
-}
